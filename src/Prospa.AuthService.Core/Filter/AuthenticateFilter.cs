1962c0e973d971035ff5312d733e3c60e0bf7e8c
ugwum <obieziefule@gmail.com>
2024-03-04 08:31:12 +0100

    Implement Request Filters

---
+++ src/Prospa.AuthService.Core/Filter/AuthenticateFilter.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using Prospa.AuthService.Core.Infrastructure;
+using Prospa.AuthService.Core.Infrastructure.Abstractions;
+using Prospa.AuthService.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prospa.AuthService.Core.Filter
+{
+    public class AuthClientAuthorizationFilter : IAsyncAuthorizationFilter
+    {
+        private readonly ILogger<AuthClientAuthorizationFilter> _logger;
+        private readonly IAuthClientManager _authClientManager;
+
+        public AuthClientAuthorizationFilter(ILogger<AuthClientAuthorizationFilter> logger, IAuthClientManager authClientManager)
+        {
+            _logger = logger;
+            _authClientManager = authClientManager; 
+        }
+
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            try
+            {
+                var allowAnonymous = context.ActionDescriptor.EndpointMetadata.Any(em => em.GetType().Name == "AllowAnonymousAttribute");
+                if (allowAnonymous)
+                    return;
+
+                var clientId = context.HttpContext.Request.Headers["ClientID"].FirstOrDefault()?.Split(" ").Last();
+                var signature = context.HttpContext.Request.Headers["Signature"].FirstOrDefault()?.Split(" ").Last();
+
+                if (string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(signature))
+                {
+                    context.Result = new JsonResult(new { code = "INVALID_AUTHCLIENT_CREDENTIAL", message = "ClientID or Signature missing in the header" })
+                    {
+                        StatusCode = (int)HttpStatusCode.BadRequest
+                    };
+                    return;
+                }
+
+                var authClientIsValid = await _authClientManager.VerifyAuthServiceClient(clientId, signature);
+
+                if (!authClientIsValid)
+                {
+                    context.Result = new JsonResult(new { code = "INVALID_AUTHCLIENT", message = "Auth client is not valid" })
+                    {
+                        StatusCode = (int)HttpStatusCode.Forbidden
+                    };
+                    return;
+                }
+
+            }
+            catch (CustomException ex)
+            {
+                _logger.LogError($"An error occurred Code {ex.code}, {ex.Message}, {ex.StackTrace}");
+                context.Result = await HandleCustomExceptionAsync(ex);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred {ex.Message}, {ex.StackTrace}");
+                context.Result = await HandleExceptionAsync(ex);
+            }
+        }
+
+        private static async Task<JsonResult> HandleCustomExceptionAsync(CustomException exception)
+        {
+            var data = new RequestResult { message = exception.Message, code = exception.code };
+
+            return new JsonResult(data)
+            {
+                StatusCode = (int)HttpStatusCode.Forbidden
+            };
+        }
+
+        private static async Task<JsonResult> HandleExceptionAsync(Exception exception)
+        {
+            var data = new RequestResult { message = GeneralStatusCodes.UNEXPECTED_ERROR.message, code = GeneralStatusCodes.UNEXPECTED_ERROR.code };
+
+            return new JsonResult(data)
+            {
+                StatusCode = (int)HttpStatusCode.InternalServerError
+            };
+        }
+    }
+}
---
+++ src/Prospa.AuthService.Core/Filter/JWTAuthorize.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using Prospa.AuthService.Core.Infrastructure;
+using Prospa.AuthService.Core.Infrastructure.Abstractions;
+using Prospa.AuthService.Core.Model;
+using Prospa.AuthService.Core.Service.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prospa.AuthService.Core.Filter
+{
+    public class JWTAuthorize : IAsyncAuthorizationFilter
+    {
+        private readonly IJWTService _jwtService;
+        private readonly ILogger<JWTAuthorize> _logger;
+        public JWTAuthorize(IJWTService jwtService, ILogger<JWTAuthorize> logger)
+        {
+             _jwtService = jwtService;
+            _logger = logger;
+        }
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            try
+            {
+                var allowAnonymous = context.ActionDescriptor.EndpointMetadata.Any(em => em.GetType().Name == "AllowAnonymousAttribute");
+                if (allowAnonymous)
+                    return;
+
+                string? authorizationToken = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+                var tokenIsValid = await _jwtService.IsTokenValidAsync(authorizationToken);
+
+ 
+                if (!tokenIsValid)
+                {
+                    context.Result = new JsonResult(new { code = AuthStatusCode.INVALID_AUTHTOKEN.code, message = AuthStatusCode.INVALID_AUTHTOKEN.message })
+                    {
+                        StatusCode = (int)HttpStatusCode.Forbidden
+                    };
+                    return;
+                }
+
+            }
+            catch (CustomException ex)
+            {
+                _logger.LogError($"An error occurred Code {ex.code}, {ex.Message}, {ex.StackTrace}");
+                context.Result = await HandleCustomExceptionAsync(ex);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occurred {ex.Message}, {ex.StackTrace}");
+                context.Result = await HandleExceptionAsync(ex);
+            }
+        }
+
+        #region Helper method To be refactored
+        private static async Task<JsonResult> HandleCustomExceptionAsync(CustomException exception)
+        {
+            var data = new RequestResult { message = exception.Message, code = exception.code };
+
+            return new JsonResult(data)
+            {
+                StatusCode = (int)HttpStatusCode.Forbidden
+            };
+        }
+
+        private static async Task<JsonResult> HandleExceptionAsync(Exception exception)
+        {
+            var data = new RequestResult { message = GeneralStatusCodes.UNEXPECTED_ERROR.message, code = GeneralStatusCodes.UNEXPECTED_ERROR.code };
+
+            return new JsonResult(data)
+            {
+                StatusCode = (int)HttpStatusCode.InternalServerError
+            };
+        }
+
+        #endregion
+
+    }
+}
